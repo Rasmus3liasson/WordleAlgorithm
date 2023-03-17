8fba9bdf1a5e65b73e325f0e563e94327afa9cd7
Rasmus Eliasson <ras.muse@hotmail.com>
2023-03-17 05:06:26 +0100

    start of algorithmB

---
+++ algorithmB.js
@@ -0,0 +1,29 @@
+const foodList =
+  "Biff Korn Fisk Aprikos Fläskkött Fisk Dricka Kummel Paj Päron".split(" ");
+const colorList = "Rosa Gul Blå Gul Brun Svart Vit Orange Violett".split(" ");
+const workList =
+  "Gatsopare Skomakare Skådespelerska Idrottare Advokat Snickare Servitör Entreprenör Pilot Brandman".split(
+    " "
+  );
+
+foodList.indexOf(letter, foodList.indexOf(letter) + 1) > -1;
+console.log(letter);
+
+let wordsArr = [];
+function algorithmB(category, lengthOfWord) {
+  /*  if (category == wordsArr) {
+    wordsArr.push(...foodList);
+    wordsArr.push(...colorList);
+    wordsArr.push(...workList);
+  }
+  console.log(wordsArr);
+
+  let askedVauleArr = [];
+
+  for (let i = 0; i < category.length; i++) {
+    if (category[i].length <= lengthOfWord) {
+      askedVauleArr.push(category[i]);
+    }
+  } */
+}
+algorithmB(wordsArr, 3);
